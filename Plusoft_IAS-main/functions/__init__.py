8c2d0ae28970da5e7b085aa3776cc4cc50d07dba
Marcela Abrahão <85709055+ella-at@users.noreply.github.com>
2024-10-23 22:27:59 -0300

    Add files via upload

---
+++ Plusoft_IAS-main/functions/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+from .data_transform import completa_dataframe_agricola
+
+from .data_transform import conectar_bd
+from .data_transform import inserir_cultura
+from .data_transform import atualizar_cultura
+from .data_transform import inserir_producao
+from .data_transform import exibir_culturas
+from .data_transform import exibir_producao
